1fb3086dde62e47d1daea75d84dfed5a41e10077
Adriano Lucati <adrianolucati@outlook.com>
2022-03-12 15:05:25 -0300

    first commit

---
+++ lucatilog-api/src/main/java/com/lucatiworks/lucatilog/domain/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.lucatiworks.lucatilog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Repository;
+
+import com.lucatiworks.lucatilog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepositoryImplementation<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+
+	List<Cliente> findByNomeContaining(String nome);
+}
---
+++ lucatilog-api/src/test/java/com/lucatiworks/lucatilog/LucatilogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatiworks.lucatilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucatilogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
